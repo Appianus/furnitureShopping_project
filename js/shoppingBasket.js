f74939a905cb9335fd0df715143d870b1408e0fc
appianus <appianus@gmail.com>
2022-11-22 19:19:41 +0900

    -장바구니에 카드 구현 펑션 분리. -'담기'버튼 구현

--- js/shoppingBasket.js
+++ js/shoppingBasket.js
@@ -1,6 +1,28 @@
 const buyButton = document.querySelector("#payment button");
 let totalSumPrice = document.querySelector("#sumPrice");
 
+function productToBasket(goodsId) {
+  document.querySelector("#dragInteract .center").classList.add("hidden"); //원래 있던 안내 지우기.
+  if (overlapCheck.includes(goodsId) === false) {
+    //includes로 ID를 이용해 중복체크 구간에 같은 품목이 있는가 체크.
+    const originalNod = document.getElementById(goodsId);
+    const copyNode = originalNod.cloneNode(true);
+    copyNode.id = COPYSTR + goodsId;
+    shoppingBasket.appendChild(copyNode);
+    //cloneNode로 복사한 것을 만들고, 새롭게 id를 부여해서 이동시킴.
+    overlapCheck.push(goodsId);
+    //goodsId를 넣어서 중복체크와 나중에 영수증에서 구매하는 품목 체크.
+    buttonToInput(copyNode.id);
+  } else if (overlapCheck.includes(goodsId) === true) {
+    const nodeInput = document.querySelector(
+      `#${COPYSTR}${goodsId}${INPUTSTR}`
+    );
+    const goodsAmount = Number(nodeInput.value);
+    nodeInput.value = goodsAmount + 1;
+  }
+  renewalCards();
+}
+
 function renewalCards() {
   const basketCards = document.querySelectorAll("#dragInteract .card");
   let sumGoodsPrice = 0;
